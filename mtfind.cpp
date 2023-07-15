9d3538ad0598bb4e2e131ed718a4ebae2d86b1c4
zm412 <zm412412@gmail.com>
2023-07-14 20:46:01 -0400

    change alg

--- mtfind.cpp
+++ mtfind.cpp
@@ -3,67 +3,63 @@
 #include <fstream>
 #include <string> 
 #include <iostream>
+
 using namespace std;
- 
-void KMP(string text, string pattern, int counter)
+
+int *pre_kmp(string pattern, string ticket)
 {
-    int m = text.length();
-    int n = pattern.length();
- 
-    if (n == 0)
-    {
-        cout << "The pattern occurs with shift 0";
-        return;
-    }
- 
-    if (m < n)
-    {
-        cout << "Pattern not found";
-        return;
-    }
- 
-    int next[n + 1];
- 
-    for (int i = 0; i < n + 1; i++) {
-        next[i] = 0;
-    }
- 
-    for (int i = 1; i < n; i++)
-    {
-        int j = next[i];
- 
-        while (j > 0 && pattern[j] != pattern[i]) {
-            j = next[j];
-        }
- 
-        if (j > 0 || pattern[j] == pattern[i]) {
-            next[i + 1] = j + 1;
-        }
-    }
- 
-    for (int i = 0, j = 0; i < m; i++)
-    {
-        if (text[i] == pattern[j] || pattern[j] == '?')
-        {
-            if (++j == n) {
-                cout << counter << " " << i - 1 << " " << pattern << endl;
-            }
-        }
-        else if (j > 0)
-        {
-            j = next[j];
-            i--;    
-        }
-    }
+	int size = pattern.size();
+	int *pie=new int [size];
+	pie[0] = 0;
+	int k=0;
+	for(int i=1;i<size;i++)
+	{
+		while(k>0 && pattern[k] != pattern[i])
+		{
+			k=pie[k-1];
+		}
+		if( pattern[k] == pattern[i])
+		{
+			k=k+1;
+		}
+		pie[i]=k;
+	}
+
+	return pie;
 }
- 
+
+void KMP(string text,string pattern, string ticket)
+{
+	int* pie=pre_kmp(pattern, ticket);
+	int matched_pos=0;
+	for(int current=0; current< text.length(); current++)
+	{
+		while (matched_pos > 0 && pattern[matched_pos] != text[current] && pattern[matched_pos] != ticket[0] )
+			matched_pos = pie[matched_pos-1];
+			
+		if(pattern[matched_pos] == text[current] || pattern[matched_pos] == ticket[0])
+			matched_pos = matched_pos+1;
+			
+		if( matched_pos == (pattern.length()) )
+		{
+            
+			cout<<"Pattern occurs with shift "<< current - (pattern.length() -1 ) << " " << text.substr(current - (pattern.length() -1 ), pattern.length())  << "\n";
+			matched_pos = pie[matched_pos-1];
+		}
+	}
+}
+
 int main(int argc, char* argv [])
 {
-    std::cout << argc << std::endl;
-    std::cout << argv[0] << std::endl;
+    //std::cout << argc << std::endl;
+    //std::cout << argv[0] << std::endl;
 
     std::string line;
- 
+    std::string pattern;
+    std::string ticket;
+    ticket = "?";
+    pattern = argv[2];
+
     std::ifstream in(argv[1]);
 
     if (in.is_open())
@@ -72,11 +68,12 @@ int main(int argc, char* argv [])
 
         while (std::getline(in, line))
         {
-            KMP(line, argv[2], counter);
+            KMP(line, pattern, ticket);
             counter++;
-        //    std::cout << line << std::endl;
+            //std::cout << line << std::endl;
         }
     }
-    
+
     in.close();
 }
+
